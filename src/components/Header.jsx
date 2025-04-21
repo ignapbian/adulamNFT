b631117c64aca3b05dea7bf31f230ed18fce8940
ignacio garcia <ignacioga62@gmail.com>
2025-04-21 18:43:37 +0200

    43

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,21 +9,21 @@ const Header = () => {
 
   return (
     <header className="bg-black text-white p-4">
-      <nav className="flex justify-between items-center">
-        <Link to="/" className="text-2xl font-bold">
+      <nav className="flex flex-col sm:flex-row justify-center sm:justify-between items-center">
+        <Link to="/" className="text-2xl font-bold mb-4 sm:mb-0">
           NFTBlockBrothers
         </Link>
-        <div>
-          <Link to="/" className="mr-4">
+        <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-4">
+          <Link to="/" className="sm:inline-block">
             Home
           </Link>
-          <Link to="/about" className="mr-4">
+          <Link to="/about" className="sm:inline-block">
             About Us
           </Link>
           {connectedAccount ? (
             <button
-              className="shadow-xl shadow-black text-black 
-              bg-[#ffffff] hover:bg-[#b8b8b8] md:text-xs p-2
+              className="shadow-xl shadow-black text-white 
+              bg-[#000000] hover:bg-[#fffefe] text-xs p-2
               rounded-full cursor-pointer"
             >
               {truncate(connectedAccount, 4, 4, 11)}
@@ -31,7 +31,7 @@ const Header = () => {
           ) : (
             <button
               className="shadow-xl shadow-black text-black 
-              bg-[#ffffff] hover:bg-[#b0b0b0] md:text-xs p-2
+              bg-[#ffffff] hover:bg-[#000000] text-xs p-2
               rounded-full cursor-pointer"
               onClick={connectWallet}
             >
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,38 +23,38 @@ const Hero = () => {
 
   return (
     <div
-      className="relative h-full w-full bg-no-repeat bg-center bg-fixed bg-cover"
+      className="relative h-screen w-full bg-no-repeat bg-center bg-fixed bg-cover"
       style={{ backgroundImage: `url(${background})` }}
     >
       <div className="absolute inset-0 bg-black opacity-60"></div>
-      <div className="relative z-10 flex flex-col justify-center items-center mx-auto py-20 px-8">
-        <div className="flex flex-col justify-center items-center max-w-4xl text-center space-y-8">
-          <h1 className="text-white text-6xl font-bold leading-tight mb-6 animate-fadeIn">
+      <div className="relative z-10 flex flex-col justify-center items-center mx-auto py-10 px-4 sm:px-8">
+        <div className="flex flex-col justify-center items-center max-w-2xl text-center space-y-6">
+          <h1 className="text-white text-4xl sm:text-6xl font-bold leading-tight mb-4 animate-fadeIn">
             NFTBlockBrothers <br />
             <span className="text-gradient">Unlocking Financial Freedom Through Art</span>
           </h1>
 
-          <p className="text-white font-semibold text-xl mt-4 animate-fadeIn delay-100">
+          <p className="text-white font-semibold text-lg sm:text-xl mt-2 animate-fadeIn delay-100">
             Welcome to NFTBlockBrothers, a pioneering Web3 platform where art meets empowerment.
           </p>
 
-          <p className="text-white text-lg font-medium mt-6 animate-fadeIn delay-200">
+          <p className="text-white text-base sm:text-lg font-medium mt-4 animate-fadeIn delay-200">
             Our mission is to offer collectible NFTs that embody themes of financial freedom, empowerment, and social impact. Each piece is a unique, AI-crafted digital artwork, providing not just ownership, but identity and purpose.
           </p>
 
-          {/* <p className="text-white text-lg font-medium mt-6 animate-fadeIn delay-300">
+          <p className="text-white text-base sm:text-lg font-medium mt-4 animate-fadeIn delay-300">
             We facilitate seamless transactions through our proprietary platform, supporting card payments, Apple Pay, and Google Pay, with revenue received in USDT for transparency and efficiency.
           </p>
 
-          <p className="text-white text-lg font-medium mt-6 animate-fadeIn delay-400">
+          <p className="text-white text-base sm:text-lg font-medium mt-4 animate-fadeIn delay-400">
             By purchasing an NFT from NFTBlockBrothers, you gain access to a world of exclusive benefits:
           </p>
 
-          <ul className="text-white text-lg font-medium mt-6 space-y-2 animate-fadeIn delay-500">
+          <ul className="text-white text-base sm:text-lg font-medium mt-4 space-y-2 animate-fadeIn delay-500">
             <li>- Ownership of a unique, AI-generated digital masterpiece.</li>
             <li>- A stake in a community that values independence, prosperity, and social impact.</li>
             <li>- Opportunities for future rewards and exclusive access to new collections and events.</li>
-          </ul> */}
+          </ul>
           <MyNFTs />
         </div>
       </div>
